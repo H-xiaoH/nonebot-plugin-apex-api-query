bc561b8fe3313c7edaeb0b6042066a1ef08493fd
H-xiaoH <a412454922@gmail.com>
2023-01-18 14:01:57 +0800

    添加默认 API Key

--- nonebot_plugin_apex_api_query/config.py
+++ nonebot_plugin_apex_api_query/config.py
@@ -1,6 +1,5 @@
 from pydantic import BaseModel, Extra
-from typing import Optional
 
 class Config(BaseModel, extra=Extra.ignore):
-    apex_api_key: Optional[str] = None
-    apex_api_url = 'https://api.mozambiquehe.re/'
+    apex_api_key: str = '173fd0ee53fb32d4c7063eb5bc700c9e'
+    apex_api_url: str = 'https://api.mozambiquehe.re/'
