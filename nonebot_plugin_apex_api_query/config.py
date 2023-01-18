978bf92374b2025301971a4717b48c45b3316abc
H-xiaoH <a412454922@gmail.com>
2023-01-18 14:02:46 +0800

    增加 txt2img 配置选项

--- nonebot_plugin_apex_api_query/config.py
+++ nonebot_plugin_apex_api_query/config.py
@@ -3,3 +3,4 @@ from pydantic import BaseModel, Extra
 class Config(BaseModel, extra=Extra.ignore):
     apex_api_key: str = '173fd0ee53fb32d4c7063eb5bc700c9e'
     apex_api_url: str = 'https://api.mozambiquehe.re/'
+    apex_api_t2i: bool = True
