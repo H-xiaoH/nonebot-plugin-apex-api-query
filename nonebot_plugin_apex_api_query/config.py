51e843eec8907bb5934da25dc6e3aab1153f009c
H-xiaoH <a412454922@gmail.com>
2023-10-15 15:09:20 +0800

    Update config.py

--- nonebot_plugin_apex_api_query/config.py
+++ nonebot_plugin_apex_api_query/config.py
@@ -4,4 +4,3 @@ from typing import Optional
 class Config(BaseModel, extra=Extra.ignore):
     apex_api_key: Optional[str] = None
     apex_api_url: str = 'https://api.mozambiquehe.re/'
-    apex_api_t2i: bool = True
