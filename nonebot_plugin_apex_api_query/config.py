2dae94340d50c262f1524de92016e2c83bb7eef1
H-xiaoH <a412454922@gmail.com>
2024-01-02 21:16:58 +0800

    Revert "Update config.py"
    This reverts commit 51e843eec8907bb5934da25dc6e3aab1153f009c.

--- nonebot_plugin_apex_api_query/config.py
+++ nonebot_plugin_apex_api_query/config.py
@@ -4,3 +4,4 @@ from typing import Optional
 class Config(BaseModel, extra=Extra.ignore):
     apex_api_key: Optional[str] = None
     apex_api_url: str = 'https://api.mozambiquehe.re/'
+    apex_api_t2i: bool = True
